aaeafeaa9f0d55b1a1b0928d3d7d94a633f9ea47
Paul <quazantip@gmail.com>
2021-12-07 18:12:35 +0400

    more fields added.

--- src/com/application/Cell.java
+++ src/com/application/Cell.java
@@ -1,7 +1,12 @@
 package com.application;
 
 public class Cell {
+
+    public static char ALIVE_CELL = '+';
+    public static char DEAD_CELL = '0';
     protected boolean isAlive;
     protected int x;
     protected int y;
+    char prints;
+    protected int proximityCount;
 }
