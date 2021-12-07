a60b5a10ef08424f216073fa6fe7dac5ad0f3422
Paul <quazantip@gmail.com>
2021-12-07 18:36:47 +0400

    methods added.

--- src/com/application/Cell.java
+++ src/com/application/Cell.java
@@ -1,12 +1,21 @@
 package com.application;
 
 public class Cell {
-
     public static char ALIVE_CELL = '+';
     public static char DEAD_CELL = '0';
+
     protected boolean isAlive;
     protected int x;
     protected int y;
-    char prints;
     protected int proximityCount;
+
+    public char printChar(){
+        if(isAlive) {
+            return ALIVE_CELL;
+        } else
+            return DEAD_CELL;
+    }
+
+    public Cell(int x, int y, boolean isAlive) {
+    }
 }
--- src/com/application/Screen.java
+++ src/com/application/Screen.java
@@ -2,6 +2,27 @@ package com.application;
 
 public class Screen {
 
+    public static final int SCREEN_HEIGHT = 20;
+    public static final int SCREEN_WIDTH = 20;
 
+    Cell[][] field = new Cell[SCREEN_HEIGHT][SCREEN_WIDTH];
 
+    public Screen() {
+        for (int i = 0; i < SCREEN_HEIGHT; i++) {
+            for (int j = 0; j < SCREEN_WIDTH; j++) {
+ //               System.out.println("i -- " + i);
+  //              System.out.println("j -- " + j);
+                field[i][j] = new Cell(i, j, false);
+            }
+        }
+    }
+
+    void print() {
+        for (int i = 0; i < SCREEN_HEIGHT; i++) {
+            for (int j = 0; j < SCREEN_WIDTH; j++) {
+             Cell current = field[i][j];
+                System.out.println(current.printChar());
+            }
+        }
+    }
 }
